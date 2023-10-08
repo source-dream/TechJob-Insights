ec37283de4dec817494b24301defff3bb517649b
sourcedream <96290808+source-dream@users.noreply.github.com>
2023-10-08 23:21:02 +0800

    后台用户管理

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,6 +1,7 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, jsonify
 from . import admin_bp
 from app.models import User
+from app import db
 
 @admin_bp.route("/")
 def admin():
@@ -12,4 +13,15 @@ def admin_data():
 
 @admin_bp.route("/user")
 def admin_user():
-    return render_template('admin-user.html')
+    users = User.query.all()
+    return render_template('admin-user.html', users=users)
+
+@admin_bp.route('/delete_user/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    user = User.query.get(id)
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({'message': 'User deleted successfully'})
+    else:
+        return jsonify({'message': 'User not found'}), 404
